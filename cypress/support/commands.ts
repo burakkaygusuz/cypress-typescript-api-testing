16b9914f9d588ec6e88c78cedb10631750cf4609
Burak Kaygusuz <kaygusuzburak@gmail.com>
2023-05-29 21:25:38 +0300

    Refactor custom commands

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,13 +1,13 @@
 import { Booking } from '../types';
 
-Cypress.Commands.add('healthCheck', () => {
-  cy.request('/ping').then(({ status }) => {
+const healthCheck = () => {
+  return cy.request('/ping').then(({ status }) => {
     expect(status).to.eq(201);
   });
-});
+};
 
-Cypress.Commands.add('createToken', (username: string, password: string) => {
-  cy.session([username, password], () => {
+const createToken = (username: string, password: string) => {
+  return cy.session([username, password], () => {
     cy.request({
       method: 'POST',
       url: '/auth',
@@ -19,50 +19,39 @@ Cypress.Commands.add('createToken', (username: string, password: string) => {
       cy.setCookie('token', body.token);
     });
   });
-});
+};
 
-Cypress.Commands.add(
-  'getBooking',
-  (
-    bookingId: string,
-    qs?: {
-      firstname?: string;
-      lastname?: string;
-      checkin?: string;
-      checkout?: string;
+const getBooking = (
+  bookingId: string,
+  qs?: { firstname?: string; lastname?: string; checkin?: string; checkout?: string },
+) => {
+  return cy.request({
+    method: 'GET',
+    url: `/booking/${bookingId}`,
+    qs: qs,
+  });
+};
+
+const createBooking = (booking: {
+  firstname: string;
+  lastname: string;
+  totalprice: number;
+  depositpaid: boolean;
+  bookingdates: { checkin: string; checkout: string };
+  additionalneeds: string;
+}) => {
+  return cy.request({
+    method: 'POST',
+    url: '/booking',
+    headers: {
+      'Content-Type': 'application/json',
     },
-  ) => {
-    cy.request({
-      method: 'GET',
-      url: `/booking/${bookingId}`,
-      qs: qs,
-    });
-  },
-);
+    body: booking,
+  });
+};
 
-Cypress.Commands.add(
-  'createBooking',
-  (booking: {
-    firstname: string;
-    lastname: string;
-    totalprice: number;
-    depositpaid: boolean;
-    bookingdates: { checkin: string; checkout: string };
-    additionalneeds: string;
-  }) => {
-    cy.request({
-      method: 'POST',
-      url: '/booking',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: booking,
-    });
-  },
-);
-
-Cypress.Commands.add('updateBooking', (bookingId: string, booking: Booking) => {
-  cy.request({
+const updateBooking = (bookingId: string, booking: Booking) => {
+  return cy.request({
     method: 'PUT',
     url: `/booking/${bookingId}`,
     headers: {
@@ -70,10 +59,10 @@ Cypress.Commands.add('updateBooking', (bookingId: string, booking: Booking) => {
     },
     body: booking,
   });
-});
+};
 
-Cypress.Commands.add('partialUpdateBooking', (bookingId: string, booking: Booking) => {
-  cy.request({
+const partialUpdateBooking = (bookingId: string, booking: Booking) => {
+  return cy.request({
     method: 'PATCH',
     url: `/booking/${bookingId}`,
     headers: {
@@ -81,14 +70,95 @@ Cypress.Commands.add('partialUpdateBooking', (bookingId: string, booking: Bookin
     },
     body: booking,
   });
-});
+};
 
-Cypress.Commands.add('deleteBooking', (bookingId: string) => {
-  cy.request({
+const deleteBooking = (bookingId: string) => {
+  return cy.request({
     method: 'DELETE',
     url: `/booking/${bookingId}`,
     headers: {
       'Content-Type': 'application/json',
     },
   });
-});
+};
+
+Cypress.Commands.add('healthCheck', healthCheck);
+Cypress.Commands.add('createToken', createToken);
+Cypress.Commands.add('getBooking', getBooking);
+Cypress.Commands.add('createBooking', createBooking);
+Cypress.Commands.add('updateBooking', updateBooking);
+Cypress.Commands.add('partialUpdateBooking', partialUpdateBooking);
+Cypress.Commands.add('deleteBooking', deleteBooking);
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Cypress {
+    interface Chainable {
+      /**
+       * @description Confirms whether the API is up and running.
+       * @example
+       *   cy.healthCheck()
+       * @see https://restful-booker.herokuapp.com/apidoc/index.html#api-Ping-Ping
+       */
+      healthCheck: typeof healthCheck;
+
+      /**
+       * @description Allows to create a new auth token to use for access to the PUT and DELETE /booking
+       * @param {string} username - The username to log in with.
+       * @param {string} password - The password to log in with.
+       * @example
+       *    cy.createToken('username', 'password')
+       * @see https://restful-booker.herokuapp.com/apidoc/index.html#api-Auth-CreateToken
+       */
+      createToken: typeof createToken;
+
+      /**
+       * @description Returns the ids of all the bookings that exist within the API. Can take optional query strings to search and return a subset of booking ids.
+       * @param {string} bookingId - The booking id to search for.
+       * @param {object} queryParams - The query parameters to search for.
+       * @example
+       *    cy.getBooking('1', {'firstname': 'Jim', 'lastname': 'Brown'})
+       * @see https://restful-booker.herokuapp.com/apidoc/index.html#api-Booking-GetBooking
+       */
+      getBooking: typeof getBooking;
+
+      /**
+       * @description Create a new booking in the API.
+       * @param {Booking} booking - The booking information to create.
+       * @example
+       *    cy.createBooking({"Jim", "Brown", 111, true {"2018-01-01",  "2019-01-01"}, "Breakfast"})
+       * @see https://restful-booker.herokuapp.com/apidoc/index.html#api-Booking-CreateBooking
+       */
+      createBooking: typeof createBooking;
+
+      /**
+       * @description Update a current booking.
+       * @param {string} bookingId - The booking id to update.
+       * @param {Booking} booking - The booking information to update.
+       * @example
+       *    cy.updateBooking({"James", "Brown", 111, true, {"2018-01-01",  "2019-01-01"}, "Breakfast"})
+       * @see https://restful-booker.herokuapp.com/apidoc/index.html#api-Booking-UpdateBooking
+       */
+      updateBooking: typeof updateBooking;
+
+      /**
+       * @description Updates a current booking with a partial payload.
+       * @param {string} bookingId - The booking id to update.
+       * @param {Booking} booking - The booking information to update.
+       * @example
+       *    cy.partialUpdateBooking({"James", "Brown"})
+       * @see https://restful-booker.herokuapp.com/apidoc/index.html#api-Booking-PartialUpdateBooking
+       */
+      partialUpdateBooking: typeof partialUpdateBooking;
+
+      /**
+       * @description Returns the ids of all the bookings that exist within the API. Can take optional query strings to search and return a subset of booking ids..
+       * @param {string} bookingId - The booking id to delete.
+       * @example
+       *    cy.deleteBooking("1")
+       * @see https://restful-booker.herokuapp.com/apidoc/index.html#api-Booking-DeleteBooking
+       */
+      deleteBooking: typeof deleteBooking;
+    }
+  }
+}
