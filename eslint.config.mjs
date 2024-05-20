1dd76dbd3a5182948fe2c2f04f1031db18785c51
Burak Kaygusuz <kaygusuzburak@gmail.com>
2024-05-20 10:40:46 +0300

    Fix eslint configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import { FlatCompat } from '@eslint/eslintrc';
+const compat = new FlatCompat();
+export default [
+  ...compat.config({
+    extends: ['plugin:cypress/recommended'],
+    ignorePatterns: ['node_modules'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off'
+    }
+  })
+];
