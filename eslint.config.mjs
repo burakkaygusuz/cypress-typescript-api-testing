6a8681089da58199b110f59d0e0f27f8d888ee36
Burak Kaygusuz <burakkaygusuz@outlook.com>
2024-10-14 13:54:09 +0300

    Add eslint-plugin-chai-friendly

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,2 +1,8 @@
+import pluginChaiFriendly from 'eslint-plugin-chai-friendly';
 import pluginCypress from 'eslint-plugin-cypress/flat';
-export default [pluginCypress.configs.globals, pluginCypress.configs.recommended];
+
+export default [
+  pluginCypress.configs.globals,
+  pluginCypress.configs.recommended,
+  pluginChaiFriendly.configs.recommendedFlat
+];
