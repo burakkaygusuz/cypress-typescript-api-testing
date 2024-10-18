31844a192a620ebfa840462b721b63f68fe5dc03
Burak Kaygusuz <17873574+burakkaygusuz@users.noreply.github.com>
2024-10-18 10:28:19 +0000

    Update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,12 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 import pluginChaiFriendly from 'eslint-plugin-chai-friendly';
 import pluginCypress from 'eslint-plugin-cypress/flat';
 
-export default [
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
   pluginCypress.configs.globals,
   pluginCypress.configs.recommended,
   pluginChaiFriendly.configs.recommendedFlat
-];
+);
