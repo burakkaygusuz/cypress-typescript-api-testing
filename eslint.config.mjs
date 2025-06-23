c64b1b0c507c11b491396a63bf1659114ecb74d5
Burak Kaygusuz <burakkaygusuz@outlook.com>
2025-06-23 14:37:42 +0300

    chore(eslint): refactor ESLint config to use defineConfig and add globals support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,23 @@
-import eslint from '@eslint/js';
+import js from '@eslint/js';
+import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import pluginCypress from 'eslint-plugin-cypress';
 import pluginChaiFriendly from 'eslint-plugin-chai-friendly';
-import pluginCypress from 'eslint-plugin-cypress/flat';
 
-export default [
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginCypress.configs.globals,
+export default defineConfig([
+  { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } }
+  },
+  tseslint.configs.recommended,
   pluginCypress.configs.recommended,
-  pluginChaiFriendly.configs.recommendedFlat
-];
+  pluginChaiFriendly.configs.recommendedFlat,
+  {
+    rules: {
+      'cypress/no-unnecessary-waiting': 'off'
+    }
+  }
+]);
